2ad050cb681475c3a08452349b4e8cbb600f44a7
Mercurialaste <thewalkerofabyssmail@gmail.com>
2022-12-06 13:21:35 +0300

    password

--- Interface.py
+++ Interface.py
@@ -5,32 +5,27 @@ from tkinter import *
 root = Tk()
 
 def btn_click():
-    print('''Работает...
-Запускаю рабочее пространство...''')
-    os.system("start cmd /k python workspace.py")   
+    if name.get() == 'admin':
+        if  s_name.get() == 'admin':
+            print('Запускаю рабочее пространство...')
+            os.system("start cmd /k python workspace.py")
+        else:
+            print('Не верный пароль или логин.')
+    else:
+        print('Не верный пароль или логин.')
+        
 root['bg'] = '#1e1e1e'
-root.title('Умный Конспект')
+root.title('Умный Конспект: Авторизация')
 photo = PhotoImage(file = 'noteicon.png')
 root.iconphoto(False, photo)
 root.wm_attributes('-alpha', 1)
-root.geometry('500x700')
+root.geometry('400x500')
 
 root.resizable(False, False)
 frame = Frame(root, 
                 bg='#252526',
                 )
-frame.pack()
-
-lable_1 = Label(frame, 
-                text = '''Приветствуем в приложении 
-"Умный Конспект"''',
-                anchor = 'n',
-                font = ('Times New Roman', 14, 'normal'),
-                fg = 'lightgrey', 
-                bg = '#252526',
-                width = 30,
-                height = 2, 
-                )
+frame.place(x= 60, y = 115)
 label_2 = Label(frame,
                     text = 'Имя пользователя:',
                     anchor = 'w',
@@ -45,16 +40,18 @@ label_3 = Label(frame,
                     bg = '#252526',
                     font = ('Times New Roman', 14, 'normal'),
                     )
-btn = Button(root, 
+name = Entry(frame)
+name.grid(row= 1, column= 1, stick = 'we')
+s_name = Entry(frame, show ='*')
+s_name.grid(row= 2, column= 1, stick = 'we')
+btn = Button(frame, 
                     text = 'Начать',
                     command = btn_click,
                     font = ('Times New Roman', 12, 'normal'),
                     fg = 'white',
                     bg = '#0e639c',
                     )
-lable_1.pack()
-label_2.pack()
-label_3.pack()
-btn.place(x = 180, y = 460, width = 150, height = 40)
-
+label_2.grid(row = 1, column = 0, stick = 'we')
+label_3.grid(row= 2, column= 0, stick = 'we')
+btn.grid(row = 3, column = 0, columnspan= 2, stick = 'we' )
 root.mainloop()
